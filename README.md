105e37f204f4a4fc5f9fb460de601a66fca72a8b
anna-zau <104685927+anna-zau@users.noreply.github.com>
2023-02-17 16:38:36 +0200

    commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+https://ibb.co/zSFxbdg
+
+# Получаем контакт по id
+
+https://ibb.co/D7Gbw21
+
+# Добавялем контакт
+
+https://ibb.co/zr05ZmD
+
+# Удаляем контакт
+
+https://ibb.co/1mqCjxB
---
+++ index.js
@@ -0,0 +1,32 @@
+const argv = require("yargs").argv;
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const requiredContact = await contacts.getContactById(id);
+      console.log(requiredContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const deletedContact = await contacts.removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
